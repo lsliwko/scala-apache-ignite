bc04fa7a6ec4bfa3dff9224d7dcf61f729061069
Leszek Sliwko <lsliwko@gmail.com>
2023-03-16 17:01:25 +0000

    cleanup

--- src/main/scala/com/server/config/WebSecurityConfig.scala
+++ src/main/scala/com/server/config/WebSecurityConfig.scala
@@ -1,13 +1,8 @@
 package com.server.config
 
-import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.context.annotation.{Bean, Configuration}
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
+import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.WebSecurity
 import org.springframework.security.config.annotation.web.configuration.{EnableWebSecurity, WebSecurityConfigurerAdapter}
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
-import org.springframework.security.crypto.password.PasswordEncoder
 
 @Configuration
 @EnableWebSecurity
